98ce6ef841235e8e3b41e51e8c05b11f05dad601
Adam Montlake <adammontlake@microsoft.com>
2026-01-04 15:41:14 +0200

    add kv for cost

--- terraform.tf
+++ terraform.tf
@@ -30,3 +30,20 @@ resource "azurerm_virtual_network" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
+
+resource "azurerm_key_vault" "example" {
+  name                        = "examkeyvaultadam"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = var.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Tenant ID for KV auth"
+}
