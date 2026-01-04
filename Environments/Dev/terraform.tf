6a27e586faa48908efc2bc9342432660a1044a10
Adam Montlake <adammontlake@microsoft.com>
2026-01-04 22:18:50 +0200

    move format to step 1

--- Environments/Dev/terraform.tf
+++ Environments/Dev/terraform.tf
@@ -37,7 +37,7 @@ resource "azurerm_virtual_network" "example" {
 }
 
 resource "azurerm_public_ip" "example" {
-  count               = 5
+  count               = 1
   name                = "dev-adam-example-pip-${count.index}"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
