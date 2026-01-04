89e27bda7f4c1e5298c701c0a88d44d33635d13a
Adam Montlake <adammontlake@microsoft.com>
2026-01-04 12:23:27 +0200

    test cicd

--- tera.js
+++ tera.js
@@ -1,15 +1,15 @@
-let vnet = createVpc({cidrBlock: '10.0.0.0/16'});
-let subnets = vnet.then(vnet => {
-  let subnets = {
-    'sub1': '10.0.1.0/24',
-    'sub2': '10.0.2.0/24',
-  };
-  return Object.entries(subnets).map(([name, cidr]) => createSubnet({
-    vnetId: vnet.id,
-    cidrBlock: cidr,
-    name: name,
-  }));
-});
+// let vnet = createVpc({cidrBlock: '10.0.0.0/16'});
+// let subnets = vnet.then(vnet => {
+//   let subnets = {
+//     'sub1': '10.0.1.0/24',
+//     'sub2': '10.0.2.0/24',
+//   };
+//   return Object.entries(subnets).map(([name, cidr]) => createSubnet({
+//     vnetId: vnet.id,
+//     cidrBlock: cidr,
+//     name: name,
+//   }));
+// });
 
 
 // let vnet = createVpc({cidrBlock: '10.0.0.0/16'});
